7abd4ad6006d644da240e687ab692d094172471f
Eduardo Silva <edsiper@gmail.com>
2010-08-20 20:02:26 -0400

    Add configuration file

---
+++ vendors/freelancer/config.php
@@ -0,0 +1,12 @@
+<?
+
+/* sandbox.freelancer.com token/secret (testing environment) */
+//$fl_consumer_token  = '';
+//$fl_consumer_secret = '';
+
+/* freelancer.com token/secret (production) */
+$fl_consumer_token  = '';
+$fl_consumer_secret = '';
+
+$fl_callback = 'http://yourdomain.com/Oauth/callback/?';
+$fl_test_mode = False;
