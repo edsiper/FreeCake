7cd25af115eac391f602733f517e4b91901b6062
Eduardo Silva <edsiper@gmail.com>
2010-08-20 21:05:42 -0400

    Add remote calls for users, projects and feedback to component

--- controllers/components/freelancer.php
+++ controllers/components/freelancer.php
@@ -35,24 +35,132 @@ class FreelancerComponent extends Object {
     return $this->lib->getRequestAccessToken($oauth_verifier);
   }
 
-  function get_token() {
+  function get_request_token() {
     return $this->lib->getRequestToken();
   }
   
-  function get_auth_url() {
+  function get_request_token_verifier() {
+    return $this->lib->getRequestTokenVerifier();
+  }
+
+  function get_authorize_url() {
     return $this->lib->getAuthorizeURL();
   }
 
-  /* Public methods */
-  function get_account_details() {
-    return $this->lib->getAccountDetails()->getArrayData();
+  function get_request_access_token($oauth_verifier = '') {
+    return $this->lib->getRequestAccessToken($oauth_verifier);
   }
-  
+
+  /* User methods */
+  function get_users_by_search($param = array()) {
+    return $this->lib->getUsersBySearch($param);
+  }
+
+  function get_user_feedback($param = array()) {
+    return $this->lib->getUserFeedback($param);
+  }
+
+  function get_pending_feedback($type = 'B') {
+    return $this->lib->getPendingFeedback($type);
+  }
+
+  function get_user_details($param = array()) {
+    return $this->lib->getUserDetails($param);
+  }
+
+  /* Job Methods */
   function get_job_list() {
-    return $this->lib->getJobList()->getArrayData();
+    return $this->lib->getJobList();
   }
 
   function get_my_job_list() {
-    return $this->lib->getMyJobList()->getArrayData();
+    return $this->lib->getMyJobList();
   }
+
+  function get_category_job_list() {
+    return $this->lib->getCategoryJobList();
+  }
+
+  function get_profile_info($userid) {
+    return $this->lib->getProfileInfo($userid);
+  }
+
+  /* Update user information */
+  function set_profile_info($param = array()) {
+    return $this->lib->setProfileInfo($param);
+  }
+
+  /* Employer */
+  function post_new_project($param = array()) {
+    return $this->lib->postNewProject($param);
+  }
+
+  function post_new_trial_project($param = array()) {
+    return $this->lib->postNewTrialProject($param);
+  }
+  
+  function post_new_draft_project($param = array()) {
+    return $this->lib->postNewDraftProject($param);
+  }
+
+  function choose_winner_for_project($projectid, $useridcsv) {
+    return $this->lib->chooseWinnerForProject($projectid, $useridcsv);
+  }
+
+  function get_posted_project_list($param = array()) {
+    return $this->lib->getPostedProjectList($param = array());
+  }
+
+  function invite_user_for_project($param = array()) {
+    return $this->lib->inviteUserForProject($param);
+  }
+  
+  function update_project_details($param = array()) {
+    return $this->lib->updateProjectDetails($param);
+  }
+
+  function elegible_for_trial_project() {
+    return $this->lib->elegibleForTrialProject();
+  }
+
+  function get_project_list_for_placed_bids($param = array()) {
+    return $this->lib->getProjectListForPlacedBids($param);
+  }
+
+  function place_bid_on_project($param = array()) {
+    return $this->lib->placeBidOnProject($param);
+  }
+
+  function retract_bid_from_project($projectid) {
+    return $this->lib->retractBidFromProject($projectid);
+  }
+
+  function accept_bid_won($projectid, $state = 1) {
+    return $this->lib->acceptBidWon($projectid, $state);
+  }
+
+  function request_cancel_project($param = array()) {
+    return $this->lib->requestCancelProject($param);
+  }
+
+  function post_feedback($param = array()) {
+    return $this->lib->postFeedback($param);
+  }
+
+  function post_reply_for_feedback($param = array()) {
+    return $this->lib->postReplyForFeedback($param);
+  }
+
+  function request_withdraw_feedback($param = array()) {
+    return $this->lib->requestWithdrawFeedback($param);
+  }
+
+  function get_config_version($function) {
+    return $this->lib->getConfigVersion($function);
+  }
+
+  function get_terms() {
+    return $this->lib->getTerms();
+  }
+
 }
