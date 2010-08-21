cb1ad0808e629e0b8c7c20b0ac9390a36a3129cd
Eduardo Silva <edsiper@gmail.com>
2010-08-21 09:17:36 -0400

    Return results of API in arrays

--- controllers/components/freelancer.php
+++ controllers/components/freelancer.php
@@ -53,234 +53,234 @@ class FreelancerComponent extends Object {
 
   /* User methods */
   function get_users_by_search($param = array()) {
-    return $this->lib->getUsersBySearch($param);
+    return $this->lib->getUsersBySearch($param)->getArrayData();
   }
 
   function get_user_feedback($param = array()) {
-    return $this->lib->getUserFeedback($param);
+    return $this->lib->getUserFeedback($param)->getArrayData();
   }
 
   function get_pending_feedback($type = 'B') {
-    return $this->lib->getPendingFeedback($type);
+    return $this->lib->getPendingFeedback($type)->getArrayData();
   }
 
   function get_user_details($param = array()) {
-    return $this->lib->getUserDetails($param);
+    return $this->lib->getUserDetails($param)->getArrayData();
   }
 
   /* Job Methods */
   function get_job_list() {
-    return $this->lib->getJobList();
+    return $this->lib->getJobList()->getArrayData();
   }
 
   function get_my_job_list() {
-    return $this->lib->getMyJobList();
+    return $this->lib->getMyJobList()->getArrayData();
   }
 
   function get_category_job_list() {
-    return $this->lib->getCategoryJobList();
+    return $this->lib->getCategoryJobList()->getArrayData();
   }
 
   function get_profile_info($userid) {
-    return $this->lib->getProfileInfo($userid);
+    return $this->lib->getProfileInfo($userid)->getArrayData();
   }
 
   /* Update user information */
   function set_profile_info($param = array()) {
-    return $this->lib->setProfileInfo($param);
+    return $this->lib->setProfileInfo($param)->getArrayData();
   }
 
   /* Employer */
   function post_new_project($param = array()) {
-    return $this->lib->postNewProject($param);
+    return $this->lib->postNewProject($param)->getArrayData();
   }
 
   function post_new_trial_project($param = array()) {
-    return $this->lib->postNewTrialProject($param);
+    return $this->lib->postNewTrialProject($param)->getArrayData();
   }
   
   function post_new_draft_project($param = array()) {
-    return $this->lib->postNewDraftProject($param);
+    return $this->lib->postNewDraftProject($param)->getArrayData();
   }
 
   function choose_winner_for_project($projectid, $useridcsv) {
-    return $this->lib->chooseWinnerForProject($projectid, $useridcsv);
+    return $this->lib->chooseWinnerForProject($projectid, $useridcsv)->getArrayData();
   }
 
   function get_posted_project_list($param = array()) {
-    return $this->lib->getPostedProjectList($param = array());
+    return $this->lib->getPostedProjectList($param = array())->getArrayData();
   }
 
   function invite_user_for_project($param = array()) {
-    return $this->lib->inviteUserForProject($param);
+    return $this->lib->inviteUserForProject($param)->getArrayData();
   }
   
   function update_project_details($param = array()) {
-    return $this->lib->updateProjectDetails($param);
+    return $this->lib->updateProjectDetails($param)->getArrayData();
   }
 
   function elegible_for_trial_project() {
-    return $this->lib->elegibleForTrialProject();
+    return $this->lib->elegibleForTrialProject()->getArrayData();
   }
 
   function get_project_list_for_placed_bids($param = array()) {
-    return $this->lib->getProjectListForPlacedBids($param);
+    return $this->lib->getProjectListForPlacedBids($param)->getArrayData();
   }
 
   function place_bid_on_project($param = array()) {
-    return $this->lib->placeBidOnProject($param);
+    return $this->lib->placeBidOnProject($param)->getArrayData();
   }
 
   function retract_bid_from_project($projectid) {
-    return $this->lib->retractBidFromProject($projectid);
+    return $this->lib->retractBidFromProject($projectid)->getArrayData();
   }
 
   function accept_bid_won($projectid, $state = 1) {
-    return $this->lib->acceptBidWon($projectid, $state);
+    return $this->lib->acceptBidWon($projectid, $state)->getArrayData();
   }
 
   function request_cancel_project($param = array()) {
-    return $this->lib->requestCancelProject($param);
+    return $this->lib->requestCancelProject($param)->getArrayData();
   }
 
   function post_feedback($param = array()) {
-    return $this->lib->postFeedback($param);
+    return $this->lib->postFeedback($param)->getArrayData();
   }
 
   function post_reply_for_feedback($param = array()) {
-    return $this->lib->postReplyForFeedback($param);
+    return $this->lib->postReplyForFeedback($param)->getArrayData();
   }
 
   function request_withdraw_feedback($param = array()) {
-    return $this->lib->requestWithdrawFeedback($param);
+    return $this->lib->requestWithdrawFeedback($param)->getArrayData();
   }
 
   function get_config_version($function) {
-    return $this->lib->getConfigVersion($function);
+    return $this->lib->getConfigVersion($function)->getArrayData();
   }
 
   function get_terms() {
-    return $this->lib->getTerms();
+    return $this->lib->getTerms()->getArrayData();
   }
 
   /* Payment methods */
 
   function get_account_balance_status() {
-    return $this->lib->getAccountBalanceStatus();
+    return $this->lib->getAccountBalanceStatus()->getArrayData();
   }
 
   function get_account_transaction_list($param = array()) {
-    return $this->lib->getAccountTransactionList($param);
+    return $this->lib->getAccountTransactionList($param)->getArrayData();
   }
 
   function request_withdrawal($param = array()) {
-    return $this->lib->requestWithdrawal($param);
+    return $this->lib->requestWithdrawal($param)->getArrayData();
   }
 
   function create_milestone_payment($param = array()) {
-    return $this->lib->createMilestonePayment($param);
+    return $this->lib->createMilestonePayment($param)->getArrayData();
   }
 
   function transfer_money($param = array()) {
-    return $this->lib->transferMoney($param);
+    return $this->lib->transferMoney($param)->getArrayData();
   }
 
   function request_cancel_withdrawal($withdrawal_id) {
-    return $this->lib->requestCancelWithdrawal($withdrawal_id);
+    return $this->lib->requestCancelWithdrawal($withdrawal_id)->getArrayData();
   }
 
   function cancel_milestone($transaction_id) {
-    return $this->lib->cancelMilestone($transaction_id);
+    return $this->lib->cancelMilestone($transaction_id)->getArrayData();
   }
 
   function get_account_milestone_list($type = 'Incoming', $count = 50, $page = 0) {
-    return $this->lib->getAccountMilestoneList($type, $count, $page);
+    return $this->lib->getAccountMilestoneList($type, $count, $page)->getArrayData();
   }
 
   function get_account_withdrawal_list($type = 'Incoming', $count = 50, $page = 0) {
-    return $this->lib->getAccountWithdrawalList($type, $count, $page);
+    return $this->lib->getAccountWithdrawalList($type, $count, $page)->getArrayData();
   }
 
   function request_release_milestone($transaction_id) {
-    return $this->lib->requestReleaseMilestone($transaction_id);
+    return $this->lib->requestReleaseMilestone($transaction_id)->getArrayData();
   }
 
   function release_milestone($transaction_id, $fullname) {
-    return $this->lib->releaseMilestone($transaction_id, $fullname);
+    return $this->lib->releaseMilestone($transaction_id, $fullname)->getArrayData();
   }
 
   function prepare_transfer($project_id, $amount, $to_user_id, $reason_type = 'full') {
-    return $this->lib->prepareTransfer($project_id, $amount, $to_user_id, $reason_type);
+    return $this->lib->prepareTransfer($project_id, $amount, $to_user_id, $reason_type)->getArrayData();
   }
 
   function get_balance() {
-    return $this->lib->getBalance();
+    return $this->lib->getBalance()->getArrayData();
   }
 
   function get_project_list_for_transfer() {
-    return $this->lib->getProjectListForTransfer();
+    return $this->lib->getProjectListForTransfer()->getArrayData();
   }
 
   function get_withdrawal_fees() {
-    return $this->lib->getWithdrawalFees();
+    return $this->lib->getWithdrawalFees()->getArrayData();
   }
 
   /* Notification methods */
   function get_notification() {
-    return $this->lib->getNotification();
+    return $this->lib->getNotification()->getArrayData();
   }
 
   function get_news() {
-    return $this->lib->getNews();
+    return $this->lib->getNews()->getArrayData();
   }
 
   /* Project methods */
   function search_projects($param = array()) {
-    return $this->lib->searchProjects($param = array());
+    return $this->lib->searchProjects($param)->getArrayData();
   }
 
   function get_project_fees() {
-    return $this->lib->getProjectFees();
+    return $this->lib->getProjectFees()->getArrayData();
   }
 
   function get_project_details($project_id) {
-    return $this->lib->getProjectDetails($project_id);
+    return $this->lib->getProjectDetails($project_id)->getArrayData();
   }
 
   function get_bids_details($project_id) {
-    return $this->lib->getBidsDetails($project_id);
+    return $this->lib->getBidsDetails($project_id)->getArrayData();
   }
 
   function get_public_messages($project_id) {
-    return $this->lib->getPublicMessages($project_id);
+    return $this->lib->getPublicMessages($project_id)->getArrayData();
   }
 
   function post_public_message($param = array()) {
-    return $this->lib->postPublicMessage($param);
+    return $this->lib->postPublicMessage($param)->getArrayData();
   }
 
   /* Message methods */
   function get_inbox_messages($param = array()) {
-    return $this->lib->getInboxMessages($param);
+    return $this->lib->getInboxMessages($param)->getArrayData();
   }
 
   function get_sent_messages($param = array()) {
-    return $this->lib->getSentMessages($param);
+    return $this->lib->getSentMessages($param)->getArrayData();
   }
 
   function get_unread_count() {
-    return $this->lib->getUnreadCount();
+    return $this->lib->getUnreadCount()->getArrayData();
   }
 
   function send_message($param = array()) {
-    return $this->lib->sendMessage($param);
+    return $this->lib->sendMessage($param)->getArrayData();
   }
 
   function mark_message_as_read($id) {
-    return $this->lib->markMessageAsRead($id);
+    return $this->lib->markMessageAsRead($id)->getArrayData();
   }
 
   function load_message_thread($param = array()) {
-    return $this->lib->loadMessageThread($param);
+    return $this->lib->loadMessageThread($param)->getArrayData();
   }
 }
